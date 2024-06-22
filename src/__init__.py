d3f36c666bfea98a740fb6fd4c60815a8159e3ac
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2024-06-23 00:47:48 +0530

    Add handling cors

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_cors import CORS
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('src.config.Config')
     # app.config.from_pyfile('config.py')
 
