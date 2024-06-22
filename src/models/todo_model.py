bdb7c92b7dbd76f6dfc72aeb3329f1b2181a8cee
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2024-06-22 23:39:16 +0530

    migration folder

--- src/models/todo_model.py
+++ src/models/todo_model.py
@@ -1,6 +1,7 @@
 from src import db
 
 class Todo(db.Model):
+    __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=True)
