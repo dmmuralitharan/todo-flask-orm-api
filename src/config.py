a4a73754aaef6b67d8a341b81bfffc3530ea7211
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2024-06-26 01:35:22 +0530

    Fix Migration Issue

--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
 import os
 
+DB_URL = "mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}".format(DB_USER = os.getenv("DB_USER"),DB_PASSWORD = os.getenv("DB_PASSWORD"),DB_HOST = os.getenv("DB_HOST"),DB_NAME = os.getenv("DB_NAME"))
+
+print(DB_URL)
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = DB_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
